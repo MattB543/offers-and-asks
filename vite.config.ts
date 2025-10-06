c02b4cb82ced81d60e7fa34bfcea46bdd18bcc16
Matt Brooks <matt@incognitoforslack.com>
2025-10-06 17:25:12 -0400

    Adding wrangler

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+  // Copy _headers and _redirects to dist for Cloudflare Pages
+  publicDir: "public",
+});
